210b6ed8124198e2d0f55acbe44a3878724fdacf
rahul raushan <raushanrahul23@gmail.com>
2023-07-05 19:04:43 +0530

    Book Object

--- block-BJaaij/code/script.js
+++ block-BJaaij/code/script.js
@@ -1,13 +1,13 @@
 class BookList {
   constructor() {
-    this.bookArr = bookArray;
-    this.currInd = currIndex;
+    this.bookArr = [];
+    this.currInd = 0;
   }
-  add(book) {
-    return this.bookArr.push(book);
+  add(books = []) {
+    return this.bookArr.concat(books);
   }
   getCurrentBook() {
-    return this.currInd;
+    return this.bookArr[this.currInd];
   }
   getNextBook() {
     let currBookInd = this.currInd;
@@ -19,19 +19,38 @@ class BookList {
   }
   changeCurrentBook(ind) {
     this.currInd = ind;
+    return this.currInd;
   }
 }
 
 class Book {
-  constructor() {
+  constructor(title, category, author) {
     this.title = title;
     this.category = category;
     this.author = author;
     this.isRead = false;
-    this.finishedDate = finishedDate;
+    this.finishedDate = null;
   }
   markBookAsRead() {
     this.isRead = true;
     this.finishedDate = Date.now();
   }
 }
+
+let book1 = new Book(
+  "Sapiens: A Brief History of Humankind",
+  "History",
+  "Yuval Noah Harari"
+);
+
+let book2 = new Book("The Alchemist", "Inspirational Fiction", "Paulo Coelho");
+
+let book3 = new Book(
+  "The Great Gatsby",
+  "Literary Fiction",
+  "F.Scott Fitzgerald"
+);
+
+let book4 = new Book("1984", "Dystopian Fiction", "George Orwell");
+
+let book5 = new Book("To Kill a Mockingbird", " Classic Fiction", "Harper Lee");
